93d4a60a8c774edccbdcf7ff078ec0c9c0afe6be
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2021-06-26 18:24:27 +0700

    Improved Accuracy in Detecting IP Address

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
-    echo $_SERVER['REMOTE_ADDR'];
+    if(!empty($_SERVER['HTTP_CLIENT_IP'])) echo $_SERVER['HTTP_CLIENT_IP'];
+    elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) echo $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else echo $_SERVER['REMOTE_ADDR'];
 ?>
