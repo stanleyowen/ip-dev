ecf94170991a4a250fa844a05ba532f54921fe80
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2021-06-26 20:21:34 +0700

    Support for JSON Format

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
-    if(!empty($_SERVER['HTTP_CLIENT_IP'])) echo $_SERVER['HTTP_CLIENT_IP'];
-    elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) echo $_SERVER['HTTP_X_FORWARDED_FOR'];
-    else echo $_SERVER['REMOTE_ADDR'];
+    $query = $_GET['q'];
+    
+    if(!empty($_SERVER['HTTP_CLIENT_IP'])) $ipAddr = $_SERVER['HTTP_CLIENT_IP'];
+    elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) $ipAddr = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else $ipAddr = $_SERVER['REMOTE_ADDR'];
+
+    if(!empty($query)) {
+        if ($query === 'json') echo json_encode(array('ip' => $ipAddr));
+    }else echo $ipAddr;
 ?>
